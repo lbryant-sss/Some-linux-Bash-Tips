056e5fb5e5cce8e269b18db1500612722d4f616b
Just Brian <61902485+lbryant-sss@users.noreply.github.com>
2022-04-06 09:00:44 +0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,8 @@
+#To move an installed file to the home directory
+# item:cannot find GOROOT directory: /usr/local/item
+#item = installed package that you cannot acces
+export GOROOT=$HOME/go1.X
+export PATH=$PATH:$GOROOT/bin
+#Above is a go (golang) case
+
+
